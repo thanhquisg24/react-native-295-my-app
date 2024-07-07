017d823b1760ca9a0e9234b1c4476c2565028bff
quirua <thanhquisg24@gmail.com>
2024-07-08 01:14:34 +0700

    add nativewind v2

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 import { ScreenOrientationProvider } from '@/context/screen-orientation-context';
 import { UserView } from '@/components/04/user';
@@ -7,16 +7,27 @@ import { UserView } from '@/components/04/user';
 
 // import { NhomTivi } from '@/components/04/tv-list';
 
+// export default function Login() {
+//   // const insets = useSafeAreaInsets();
+
+//   return (
+//     <ScreenOrientationProvider>
+//       <SafeAreaView style={styles.container}>
+//         {/* <Text>aaaaaaaaaaaaaaaaa</Text> */}
+//         <UserView />
+//       </SafeAreaView>
+//     </ScreenOrientationProvider>
+//   );
+// }
+
 export default function Login() {
   // const insets = useSafeAreaInsets();
 
   return (
-    <ScreenOrientationProvider>
-      <SafeAreaView style={styles.container}>
-        {/* <Text>aaaaaaaaaaaaaaaaa</Text> */}
-        <UserView />
-      </SafeAreaView>
-    </ScreenOrientationProvider>
+    <View className='flex-1 items-center justify-center bg-orange-600'>
+      <Text className='text-white font-bold text-4xl'>Open up App.js to start working on your app!</Text>
+      <StatusBar  />
+    </View>
   );
 }
 
